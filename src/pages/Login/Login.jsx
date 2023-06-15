7d491724f3d6aa69e39e8d5c8094ac9351b5c97e
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-06-15 14:05:37 +0600

    Log and Sign

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,58 +1,56 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
-import Swal from 'sweetalert2';
 import { AuthContext } from '../../providers/AuthProvider';
 import { Fade } from "react-awesome-reveal";
 import 'animate.css';
 import img from '../../../public/cool-background.png'
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { FcGoogle } from 'react-icons/fc';
+import { ImSpinner10 } from 'react-icons/im';
 
 const Login = () => {
 
-    const [disabled, setDisabled] = useState(false);
-    const { signIn } = useContext(AuthContext);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { signIn,googleLog, loading } = useContext(AuthContext);
+    const [show, setShow] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
-
+    const saveAddress = localStorage.getItem('address') || '/';
     const from = location.state?.from?.pathname || "/";
+    localStorage.setItem('address', from);
 
-    useEffect(() => {
-        loadCaptchaEnginge(6);
-    }, [])
 
-    const handleLogin = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
-
-        signIn(email, password)
+    const onSubmit = data => {
+        signIn(data.email, data.password)
             .then(result => {
                 const user = result.user;
                 console.log(user);
-                Swal.fire({
-                    title: 'User Login Successful.',
-                    showClass: {
-                        popup: 'animate__animated animate__fadeInDown'
-                    },
-                    hideClass: {
-                        popup: 'animate__animated animate__fadeOutUp'
-                    }
-                });
+                toast.success("Welcome Back!");
+                navigate(from, { replace: true });
+                reset();
+            })
+            .catch(err => {
+                console.log(err.message);
+                toast.error(err.message);
+            });
+    };
+
+    const handleGoogleSignIn = () => {
+        googleLog()
+            .then(result => {
+                console.log(result.user);
+                saveUser(result.user);
+                toast.success("Welcome Back!");
                 navigate(from, { replace: true });
             })
-    }
-    const handleValidateCaptcha = (e) => {
-        const user_captcha_value = e.target.value;
-        if (validateCaptcha(user_captcha_value)) {
-            setDisabled(false);
-        }
-        else {
-            setDisabled(true);
-        }
-    }
+            .catch(err => {
+                console.log(err.message);
+                toast.error(err.message);
+            });
+    };
+
 
     return (
         <>
@@ -70,33 +68,39 @@ const Login = () => {
                         </Fade>
                     </div>
                     <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleLogin} className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 ng-untouched ng-pristine ng-valid">
+                            <div className="space-y-4">
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
+                                    <input type="email" {...register("email", { required: true })} name="email" id="email" placeholder="Enter Your Email Here" className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-indigo-500 bg-gray-200 text-gray-900" data-temp-mail-org="0" />
+                                    {errors.email && <span className="text-red-600">Email is required</span>}
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block text-sm mb-2">Password</label>
+                                    <input type={show ? "text" : "password"} {...register("password", { required: true })} name="password" id="password" placeholder="*******" className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-indigo-500 bg-gray-200 text-gray-900" />
+                                    <Link className="mt-1 flex justify-end text-sm hover:underline hover:text-indigo-500 text-gray-600" onClick={() => setShow(!show)}>
+                                        {
+                                            show ? <span>Hide Password</span> : <span>Show Password</span>
+                                        }
+                                    </Link>
+                                    {errors.password && <span className="text-red-600">Password is required</span>}
+                                </div>
                             </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <LoadCanvasTemplate />
-                                </label>
-                                <input onBlur={handleValidateCaptcha} type="text" name="captcha" placeholder="type the captcha above" className="input input-bordered" />
-
-                            </div>
-                            <div className="form-control mt-6">
-                                <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
+                            <div>
+                                <button type="submit" className="bg-indigo-500 w-full rounded-md py-3 text-white">
+                                    {loading ? <ImSpinner10 className="m-auto animate-spin" size={24} /> : "Login"}
+                                </button>
                             </div>
                         </form>
+                        <div className="flex items-center pt-4 space-x-1">
+                            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+                            <p className="px-3 text-sm dark:text-gray-400">Login with social accounts</p>
+                            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+                        </div>
+                        <div onClick={handleGoogleSignIn} className="flex justify-center items-center space-x-2 border m-3 p-2 bg-indigo-100 border-gray-300 hover:border-indigo-500 border-rounded cursor-pointer">
+                            <FcGoogle size={32} />
+                            <p>Continue with Google</p>
+                        </div>
                         <p className='p-8 pt-0'><small>New Here? <Link to="/signup">Create an account.</Link> </small></p>
                     </div>
                 </div>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,106 +1,130 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../providers/AuthProvider';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { Helmet } from 'react-helmet';
 
 const SignUp = () => {
 
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
-    const { createUser, updateUserData } = useContext(AuthContext);
+    const { createUser, googleLog, updateUserData, logOut } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [show, setShow] = useState(false);
+    const location = useLocation();
+    const saveAddress = localStorage.getItem('address') || '/';
+    const from = location.state?.from?.pathname || saveAddress;
+    localStorage.setItem('address', from);
 
     const onSubmit = data => {
-        console.log(data);
 
         createUser(data.email, data.password)
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                Swal.fire({
-                    position: 'top-center',
-                    icon: 'success',
-                    title: 'Account Open Successfully',
-                    showConfirmButton: false,
-                    timer: 1500
-                })
-                // form.reset('');
                 updateUserData(data.name, data.photoURL)
-                .then(() => {
-                    console.log('User name and photo updated');
-                })
-                .catch(error => {
-                    console.log(error)
-                });
-                logOut();
-        })
-        .then(error => {
-            console.log(error)
-    })
-}
+                    .then(() => {
+                        saveUser(result.user);
+                        toast.success("User Created!");
+                        reset();
+                        logOut();
+                    })
+                    .catch((err) => {
+                        console.log(err.message);
+                        toast.error(err.message);
+                    });
+            });
+    };
+    const handleGoogleSignIn = () => {
+        googleLog()
+            .then((result) => {
+                console.log(result.user);
+                saveUser(result.user);
+                toast.success("Welcome Back!");
+                navigate(from, { replace: true });
+            })
+            .catch((err) => {
+                console.log(err.message);
+                toast.error(err.message);
+            });
+    };
 
-return (
-    <>
-        <Helmet>
-            <title>Bistro Boss | Sign Up</title>
-        </Helmet>
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-                <div className="text-center lg:text-left w-1/2">
-                    <h1 className="text-5xl font-bold">Sign up now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
-                            {errors.name && <span className="text-red-600">Name is required</span>}
+
+    return (
+        <>
+            <Helmet>
+                <title>Bistro Boss | Sign Up</title>
+            </Helmet>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="text-center lg:text-left w-1/2">
+                        <h1 className="text-5xl font-bold">Sign up now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
+                                {errors.name && <span className="text-red-600">Name is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                                {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                                {errors.email && <span className="text-red-600">Email is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password"  {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/
+                                })} placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase , one special character.</p>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                                <Link className="mt-1 flex justify-end text-sm hover:underline hover:text-indigo-500 text-gray-600" onClick={() => setShow(!show)}>
+                                    {
+                                        show ? <span>Hide Password</span> : <span>Show Password</span>
+                                    }
+                                </Link>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Sign Up" />
+                            </div>
+                        </form>
+                        <div className="flex items-center pt-4 space-x-1">
+                            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+                            <p className="px-3 text-sm dark:text-gray-400">Signup with social accounts</p>
+                            <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Photo URL</span>
-                            </label>
-                            <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
-                            {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                        <div onClick={handleGoogleSignIn} className="flex justify-center items-center space-x-2 border m-3 p-2 bg-indigo-100 border-gray-300 hover:border-indigo-500 border-rounded cursor-pointer">
+                            <FcGoogle size={32} />
+                            <p>Continue with Google</p>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
-                            {errors.email && <span className="text-red-600">Email is required</span>}
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password"  {...register("password", {
-                                required: true,
-                                minLength: 6,
-                                maxLength: 20,
-                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/
-                            })} placeholder="password" className="input input-bordered" />
-                            {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
-                            {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
-                            {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase , one special character.</p>}
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <input className="btn btn-primary" type="submit" value="Sign Up" />
-                        </div>
-                    </form>
-                    <p className='mx-auto mb-4'><small>Already have an account <Link to="/login">Login</Link></small></p>
+                        <p className='mx-auto mb-4'><small>Already have an account <Link to="/login">Login</Link></small></p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
 };
 
 export default SignUp;
